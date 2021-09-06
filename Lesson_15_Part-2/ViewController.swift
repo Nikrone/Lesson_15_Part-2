af95e46ef7b436cc3185e4cc546e87ecd7316e68
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-09-06 20:38:33 +0300

    Initial Commit

---
+++ Lesson_15_Part-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_15_Part-2
+//
+//  Created by Evgeniy Nosko on 6.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
